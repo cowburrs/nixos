a87f89178a93d0caa07f13094c110865e5cabd4c
Your Name <you@example.com>
2026-01-31 17:14:55 +1100

    Flatpak doesn't work lol

--- burrs/extra/flatpak.nix
+++ burrs/extra/flatpak.nix
@@ -2,12 +2,12 @@
 
 {
   services.flatpak.enable = true;
-  systemd.services.flatpak-repo = {
-    wantedBy = [ "multi-user.target" ];
-    path = [ pkgs.flatpak ];
-    script = ''
-      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-    '';
-	# flatpak remote-add --if-not-exists flathub org.vinegarhq.Sober
-  };
+	#  systemd.services.flatpak-repo = {
+	#    wantedBy = [ "multi-user.target" ];
+	#    path = [ pkgs.flatpak ];
+	#    script = ''
+	#      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+	#    '';
+	# # flatpak remote-add --if-not-exists flathub org.vinegarhq.Sober
+	#  };
 }
