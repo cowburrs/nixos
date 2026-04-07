3ea20db1c39c1945b1312b2e7043dca137540380
Your Name <you@example.com>
2026-04-07 21:57:52 +1000

    gitshit

--- burrs/core/services.nix
+++ burrs/core/services.nix
@@ -2,7 +2,4 @@
 
 {
   services.desktopManager.plasma6.enable = true;
-	# services.xserver.desktopManager.cinnamon.enable = true;
-	# services.xserver.desktopManager.lxqt.enable = true;
-	# services.xserver.desktopManager.xfce.enable = true;
 }
