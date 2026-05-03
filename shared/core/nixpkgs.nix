9b78a1b1d17f14bada46258c7be86fc60b9fa0d7
Your Name <you@example.com>
2026-05-03 11:08:57 +1000

    feat: no need for bullshit

--- shared/core/nixpkgs.nix
+++ shared/core/nixpkgs.nix
@@ -8,13 +8,4 @@
 {
   # Allow funky licenses
   nixpkgs.config.allowUnfree = true;
-  nixpkgs.overlays = [
-    (final: prev: {
-      nh = prev.nh.overrideAttrs (old: {
-        postPatch = (old.postPatch or "") + ''
-          substituteInPlace src/search.rs --replace-fail 'latest-44-' 'latest-46-'
-        '';
-      });
-    })
-  ];
 }
