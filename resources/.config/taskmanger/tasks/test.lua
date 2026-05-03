047951fa5fe336d31cbf2417b18757c82929d4a2
Your Name <you@example.com>
2026-05-03 19:07:49 +1000

    feat: changed ./tasks

--- resources/.config/taskmanger/tasks/test.lua
+++ resources/.config/taskmanger/tasks/test.lua
@@ -31,7 +31,9 @@ return {
 	yt("Wash Backpack"),
 	-- m.multiWeekTask("exercise", { 0, 1, 3, 5, 6, 7 }),
 	m.multiWeekTask("exercise", { 0, 1, 3, 5, 6}),
-	m.nthWeekTask("Pillow sheets", 3),
+	m.nthWeekTask("clean Pillow/Sheets", 3),
+	m.nthWeekTask("clean dakimura...", 6),
+	m.nthWeekTask("clean blanket", 12),
 	st({
 		"Test works",
 		"bin scripts instead of hypr nixos",
