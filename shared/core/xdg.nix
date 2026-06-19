2526329d6a78fae1cd1a424bec69c8353ddf2da4
Your Name <you@example.com>
2026-06-19 16:11:06 +1000

    feat: YAZI FILE MANAGEMENT LETS GOOO

--- shared/core/xdg.nix
+++ shared/core/xdg.nix
@@ -6,7 +6,9 @@
       extraPortals = with pkgs; [
         xdg-desktop-portal-wlr
         xdg-desktop-portal-gtk
+        xdg-desktop-portal-termfilechooser
       ];
     };
   };
+  xdg.portal.config.common."org.freedesktop.impl.portal.FileChooser" = [ "termfilechooser" ];
 }
