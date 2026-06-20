b0146a2d3829372538be82e08100f5e9de9fb7b3
Your Name <you@example.com>
2026-06-20 11:53:01 +1000

    refactor: xdg makes a bit more sense

--- shared/core/xdg.nix
+++ shared/core/xdg.nix
@@ -1,14 +1,13 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 {
   xdg = {
     portal = {
       enable = true;
       extraPortals = with pkgs; [
-        xdg-desktop-portal-wlr
         xdg-desktop-portal-gtk
         xdg-desktop-portal-termfilechooser
       ];
+      config.common."org.freedesktop.impl.portal.FileChooser" = [ "termfilechooser" ];
     };
   };
-  xdg.portal.config.common."org.freedesktop.impl.portal.FileChooser" = [ "termfilechooser" ];
 }
