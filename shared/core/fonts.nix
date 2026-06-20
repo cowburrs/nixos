e908a78c76d43da33be48a4b06b89973892d1e63
Your Name <you@example.com>
2026-06-20 14:40:09 +1000

    feat: exo2 is officially packaged

--- shared/core/fonts.nix
+++ shared/core/fonts.nix
@@ -1,13 +1,18 @@
-{ config, pkgs, ... }:
+{ inputs, pkgs, ... }:
 
 {
   fonts.fontDir.enable = true;
-  fonts.packages = with pkgs; [
-    nerd-fonts.jetbrains-mono # you don't need actual jetbrains mono afaik if you have this font
-    aporetic
-		fantasque-sans-mono
-		noto-fonts
-		comfortaa
-		public-sans # The university font
-  ];
+  fonts.packages =
+    with pkgs;
+    [
+      nerd-fonts.jetbrains-mono # you don't need actual jetbrains mono afaik if you have this font
+      aporetic
+      fantasque-sans-mono
+      noto-fonts
+      comfortaa
+      public-sans # The university font
+    ]
+    ++ [
+      inputs.self.packages.${pkgs.system}.exo2
+    ];
 }
