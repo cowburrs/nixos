92c6a141d2a99c067d9acf6aa6be9060cc555b98
Your Name <you@example.com>
2026-07-02 19:07:45 +1000

    feat: shut the fuck up firefox

--- resources/autoConfig.js
+++ resources/autoConfig.js
@@ -14,6 +14,8 @@ lockPref("font.name.monospace.x-western", "Aporetic Sans Mono");
 lockPref("browser.display.use_document_fonts", 0);
 lockPref("signon.rememberSignons", false);
 lockPref("widget.use-xdg-desktop-portal.file-picker", 1);
+lockPref("browser.shell.checkDefaultBrowser", false);
+
 
 // AI......
 lockPref("browser.ml.enable", false); // general switch for machine learning features in Firefox (https://www.reddit.com/r/firefox/comments/1obbrvz/how_to_completely_get_rid_of_the_ai_stuff/nki10g9/), though it might not completely disable all features (https://bugzilla.mozilla.org/show_bug.cgi?id=1971973#c11)
